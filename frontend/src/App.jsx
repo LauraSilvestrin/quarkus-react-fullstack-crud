2f663c82ba2ae75f3adea12e503f66b0a927d9d8
Silvestrin <silvestrineschembach@gmail.com>
2024-04-03 21:11:29 -0300

    ajustes frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import '../src/components/Static/Css/main.css';
-import '../src/components/Static/Css/button.css';
-import '../src/components/Static/Css/settings.css'
-import '../src/components/Button/floatButton.css'
 import Card from "./components/Card/Card";
 import { FaPlus } from "react-icons/fa";
 
